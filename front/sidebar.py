27823619f0df603e1a1ecfcb7a5f6f7d61f99efc
AlexAuragan <alexauragan@gmail.com>
2025-07-28 22:20:17 +0200

    Typo

--- front/sidebar.py
+++ front/sidebar.py
@@ -4,7 +4,7 @@ def sidebar():
     with st.sidebar:
         st.title("Frataga")
         st.selectbox(options=["frataga", "greek_gods"], index=1, key="collection", label="Collection sélectionnée",
-                     format_func=lambda x: {"frataga": "Frtaga", "greek_gods": "Greek Gods"}[x])
+                     format_func=lambda x: {"frataga": "Frataga", "greek_gods": "Greek Gods"}[x])
         st.page_link("main.py", label="Accueil", icon=":material/home:")
         st.page_link("pages/gallery.py", label="Gallerie", icon=":material/gallery_thumbnail:")
         st.divider()
